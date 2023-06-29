71f4422bedbb3bdde9accc1736cbd79735b80d73
alindamay <129402503+alindamay@users.noreply.github.com>
2023-06-29 22:54:56 +0700

    Add files via upload

---
+++ js/1 sintaks, variable, & tipe data/1variable.mjs
@@ -0,0 +1,29 @@
+//semua dalam satu baris
+var brand = "honda",
+  type = "mpv",
+  numberOfWheels = 4,
+  price;
+
+//baris terpisah (lebih baik daripada satu baris)
+var brand = "honda",
+  type = "mpv",
+  numberOfWheels = 4,
+  price;
+
+//notasi literal
+var a = [1, 2, 3];
+var obj = {};
+var n = 123;
+
+//notasi objek (tidak disarankan)
+var a = new Array(1,2,3);
+var obj = new Object();
+var n = new Number(123);
+
+console.log(brand);
+console.log(type);
+console.log(numberOfWheels);
+console.log(price);
+console.log(a);
+console.log(obj);
+console.log(n);
